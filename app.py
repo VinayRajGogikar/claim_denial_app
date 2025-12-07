e5f0b26c076c8d2cd4ea1d64745bbb48189dff32
Vinay Raj Gogikar <vinayraj9819@gmail.com>
2025-12-07 02:16:10 -0600

    Update comments for clarity in load_data function

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
 @st.cache_data
 def load_data():
     patients = pd.read_csv("data/patients_filtered.csv")
     encounters = pd.read_csv("data/encounters_filtered.csv")
 
-    # claims_filtered.csv (46 MB) loaded from Google Drive
+    # claims_filtered.csv from Google Drive
     claims = pd.read_csv(
         "https://drive.google.com/uc?export=download&id=1SgsAesNi3SHouEtESiNnhY0KdFkvXsr9"
     )
 
-    # claims_transactions_filtered.csv (500 MB) loaded from Google Drive
+    # claims_transactions_filtered.csv from Google Drive
     transactions = pd.read_csv(
         "https://drive.google.com/uc?export=download&id=1CXiodxDFeTDxGc0iyIovXY2BDekHtKtd"
     )
 
     payer_trans = pd.read_csv("data/payer_transitions_filtered.csv")
-
     return patients, encounters, claims, transactions, payer_trans
+
