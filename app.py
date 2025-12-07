d872ca9ac9a5248b28c81e34a64fd28f69936bc8
Vinay Raj Gogikar <vinayraj9819@gmail.com>
2025-12-07 02:21:47 -0600

    Clean up load_data function by removing comments
    Removed commented-out code for loading claims and transactions.

--- app.py
+++ app.py
@@ -6,17 +6,6 @@ import plotly.express as px
 def load_data():
     patients = pd.read_csv("data/patients_filtered.csv")
     encounters = pd.read_csv("data/encounters_filtered.csv")
-
-    # claims_filtered.csv from Google Drive
     claims = pd.read_csv(
         "https://drive.google.com/uc?export=download&id=1SgsAesNi3SHouEtESiNnhY0KdFkvXsr9"
     )
-
-    # claims_transactions_filtered.csv from Google Drive
-    transactions = pd.read_csv(
-        "https://drive.google.com/uc?export=download&id=1CXiodxDFeTDxGc0iyIovXY2BDekHtKtd"
-    )
-
-    payer_trans = pd.read_csv("data/payer_transitions_filtered.csv")
-    return patients, encounters, claims, transactions, payer_trans
-
