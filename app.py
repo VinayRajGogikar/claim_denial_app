ccc4983aa36a0c5277026605318d62da405811f6
Vinay Raj Gogikar <vinayraj9819@gmail.com>
2025-12-07 02:12:02 -0600

    Implement load_data function to load datasets
    Load data from CSV files and Google Drive links.

---
+++ app.py
@@ -0,0 +1,18 @@
+@st.cache_data
+def load_data():
+    patients = pd.read_csv("data/patients_filtered.csv")
+    encounters = pd.read_csv("data/encounters_filtered.csv")
+
+    # claims_filtered.csv (46 MB) loaded from Google Drive
+    claims = pd.read_csv(
+        "https://drive.google.com/uc?export=download&id=1SgsAesNi3SHouEtESiNnhY0KdFkvXsr9"
+    )
+
+    # claims_transactions_filtered.csv (500 MB) loaded from Google Drive
+    transactions = pd.read_csv(
+        "https://drive.google.com/uc?export=download&id=1CXiodxDFeTDxGc0iyIovXY2BDekHtKtd"
+    )
+
+    payer_trans = pd.read_csv("data/payer_transitions_filtered.csv")
+
+    return patients, encounters, claims, transactions, payer_trans
